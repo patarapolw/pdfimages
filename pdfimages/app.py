695609e8ddebfcdfbaabda9df1c463f2440d7690
patarapolw <patarapolw@gmail.com>
2018-09-12 12:44:05 +0700

    Add Poppler-based image extract

--- pdfimages/app.py
+++ pdfimages/app.py
@@ -2,9 +2,11 @@ import PyPDF2
 import os
 import re
 from PIL import Image
-import PIL.ImageOps
 from pathlib import Path
 from io import BytesIO
+import subprocess
+from tempfile import TemporaryDirectory
+import shutil
 
 from .util import remove_alpha, do_invert
 
@@ -26,10 +28,7 @@ class PdfImages:
         self.f.close()
 
     def get_toc(self):
-        for outline in self.reader.getOutlines():
-            self._read_outlines(outline)
-
-        toc2 = sorted(self._toc.items(), key=lambda x: x[1])
+        toc2 = self._get_toc_page()
 
         used_img = set()
         for i in range(len(toc2)-1):
@@ -38,6 +37,12 @@ class PdfImages:
             used_img.update(self.get_images(toc2[i][1], toc2[i+1][1]).keys())
 
         return self.toc
+
+    def _get_toc_page(self):
+        for outline in self.reader.getOutlines():
+            self._read_outlines(outline)
+
+        return sorted(self._toc.items(), key=lambda x: x[1])
 
     def _read_outlines(self, outlines, recursion=0):
         if isinstance(outlines, list):
@@ -52,6 +57,8 @@ class PdfImages:
                 self._title.append(title)
 
             self._toc[tuple(self._title[:recursion+1])] = self.reader.getDestinationPageNumber(outlines)
+
+        self._toc[('numPages',)] = self.reader.getNumPages()
 
     def get_images(self, start, end):
         images = dict()
@@ -68,7 +75,7 @@ class PdfImages:
 
         return images
 
-    def extract_images(self, output_folder=None, invert=False, no_alpha=False):
+    def extract_images_pypdf2(self, output_folder=None, invert=False, no_alpha=False):
         """
         From https://stackoverflow.com/a/34116472/9023855
         Requires https://github.com/mstamy2/PyPDF2.git
@@ -137,3 +144,39 @@ class PdfImages:
 
                     except AssertionError:
                         pass
+
+    def extract_images_poppler(self, output_folder=None, output_format=None, min_file_size=50000):
+        if output_folder is None:
+            output_folder = os.path.splitext(self.filename)[0]
+
+        toc2 = self._get_toc_page()
+        with TemporaryDirectory() as tempdir:
+            command = [
+                'pdfimages',
+                '-p',
+                self.filename,
+                os.path.join(tempdir, 'img')
+            ]
+
+            if output_format:
+                command[2:2] = '-' + output_format
+
+            subprocess.call(command)
+
+            for i in range(len(toc2)-1):
+                dir_names = toc2[i][0]
+                print(dir_names)
+
+                for filename in os.listdir(tempdir):
+                    src = os.path.join(tempdir, filename)
+                    if os.path.getsize(src) < min_file_size:
+                        os.unlink(src)
+                        continue
+
+                    page_number = re.search(r'(\d+)-\d+', filename).group(1)
+                    if int(page_number) in range(toc2[i][1], toc2[i+1][1]):
+                        file_root = os.path.join(output_folder, *dir_names)
+                        Path(file_root).mkdir(parents=True, exist_ok=True)
+
+                        shutil.move(src=src,
+                                    dst=os.path.join(file_root, filename))
--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 import os
-from PIL import Image
+# from PIL import Image
 
 from pdfimages.app import PdfImages
-from pdfimages.util import do_invert, remove_alpha
+# from pdfimages.util import do_invert, remove_alpha
 
 
 if __name__ == '__main__':
-    # with PdfImages(os.path.join('/Users/patarapolw/Desktop',
-    #                             'Differential Diagnosis in Surgical Pathology, 3rd Edition.pdf')) as pdf:
-    #     pdf.extract_images(no_alpha=False)
+    with PdfImages(os.path.join('/Users/patarapolw/Desktop',
+                                'Robbins and Cotran Pathologic Basis of Disease 9e - Kumar, Abbas, Aster (Elsevier 2015).pdf')) as pdf:
+        pdf.extract_images_poppler()
 
-    img = Image.open(os.path.join('/Users/patarapolw/Desktop/Differential Diagnosis in Surgical Pathology, 3rd Edition/Chapter 9/b\'Adrenal Gland\'/Differential Diagnos', '447-Im728.jpg'))
-    img = do_invert(remove_alpha(img))
-    img.save('test.jpg')
+    # img = Image.open(os.path.join('/Users/patarapolw/Desktop/Differential Diagnosis in Surgical Pathology, 3rd Edition/Chapter 9/b\'Adrenal Gland\'/Differential Diagnos', '447-Im728.jpg'))
+    # img = do_invert(remove_alpha(img))
+    # img.save('test.jpg')
